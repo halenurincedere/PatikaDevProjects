f9aaf1bcfb6ea1157585475c063938c582274ea7
Halenur İncedere <halenurincedere@gmail.com>
2024-12-24 20:00:13 +0300

    Comparison of While and Do-While Loops

---
+++ Week2/Karsilastirma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Karsilastirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Döngüsü
+            int sayac = 0;
+            Console.Write("Limit değeri giriniz: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            while (sayac <= limit)
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                sayac++;
+            }
+            //Do While Döngüsü
+            int sayac2 = 0;
+            Console.Write("Limit değeri giriniz: ");
+            int limit2 = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                sayac2++;
+            } 
+            while (sayac2 <= limit2);
+        }
+        
+        // While Döngüsü ve Do While Döngüsü arasındaki farklar : 
+        // While döngüsünde istenen koşul sağlanmadığı sürece döngü çalışmaz.
+        // Do While döngüsünde ise döngü en az bir kere çalışır ve koşul sağlanmadığı sürece döngü çalışmaya devam eder.
+    }
+}
---
+++ Week2/Karsilastirma/README.md
@@ -0,0 +1,8 @@
+# While Vs Do While Karşılaştırması
+
+## İstenen Ödev
+
+- Kullanıcının girdiği bir limit değerine kadar 0'dan bir sayaç çalıştırıp artırarak, ekrana " Ben bir Patika'lıyım " yazdırınız.
+- ( Sayacınız 0'dan başlayarak 1'er 1'er artacak, koşulunuz ( Sayac <= Limit ) )
+- Örnek inputlar : 10 ve -5
+- Uygulama testi sonrası while ve do-while arasındaki farkı yorum satırı olarak kodunuzun altına ekleyiniz.
