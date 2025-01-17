9e0f58cdc72b9df9e5cbe3fbc20f25181d96100e
Halenur İncedere <halenurincedere@gmail.com>
2025-01-17 22:07:45 +0300

    Week 5: List 1 Project completed.

---
+++ Week5/GalaGuests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a list to store the names of the guests
+        List<string> guests = new List<string> 
+        { 
+            "Bülent Ersoy", 
+            "Ajda Pekkan", 
+            "Ebru Gündeş", 
+            "Hadise", 
+            "Hande Yener", 
+            "Tarkan", 
+            "Funda Arar", 
+            "Demet Akalın" 
+        };
+
+        // Print the names of the guests
+        Console.WriteLine(" Patika Plus Gala Guests ");
+        for (int i = 0; i < guests.Count; i++)
+        {
+            Console.WriteLine($"{i + 1} - {guests[i]}");
+        }
+    }
+}
---
+++ Week5/GalaGuests/README.md
@@ -0,0 +1,20 @@
+# List 1 - Patika Plus Gala Guests
+
+## 📋 Project Requirements
+In this practice, you are asked to create a list that contains the names of the visitors. Then, you need to loop through this list using either a for or foreach loop and print each name on the screen one by one
+
+## 🔄 How the Project Works
+
+Console Output
+
+```bash
+** Patika Plus Gala Guests **
+1 - Bülent Ersoy
+2 - Ajda Pekkan
+3 - Ebru Gündeş
+4 - Hadise
+5 - Hande Yener
+6 - Tarkan
+7 - Funda Arar
+8 - Demet Akalın
+```
