5106d07d54f8509c0525ba1ccc0214b82455edbc
Halenur İncedere <halenurincedere@gmail.com>
2025-01-19 22:00:58 +0300

    Week 5: Try Catch Project completed.

---
+++ Week5/Try_Catch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Please enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"The square of the number is: {number * number}");
+                break; 
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+        }
+    }
+}
---
+++ Week5/Try_Catch/README.md
@@ -0,0 +1,19 @@
+# Try Catch
+This project is a simple try-catch example in C#. It asks the user to enter a number, calculates the square of the number, and shows an error message if the input is invalid.
+
+## 📋 Features:
+
+- Handles user input safely using try-catch.
+- Displays the square of valid numbers.
+- Shows an error message for invalid inputs.
+
+## 🔄 How the Project Works
+Example Output:
+
+```bash
+Please enter a number: a
+Invalid input! Please enter a valid number.
+
+Please enter a number: 32
+The square of the number is: 1024
+```
