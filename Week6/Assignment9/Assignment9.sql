7ed44b5c7c81380a2a7c1f042f1c053a23cae6ec
Halenur İncedere <halenurincedere@gmail.com>
2025-01-26 11:33:13 +0300

    Week 6: Assignment 9 completed.

---
+++ Week6/Assignment9/Assignment9.sql
@@ -0,0 +1,12 @@
+-- Soru 1: City tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country FROM city 
+JOIN country ON country.country_id = city.country_id;
+
+-- Soru 2: Customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id;
+
+-- Soru 3: Customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name 
+FROM rental
+JOIN customer ON customer.customer_id = rental.customer_id;
---
+++ Week6/Assignment9/README.md
@@ -0,0 +1,11 @@
+# SQL Exercises 9
+
+This project includes SQL queries using the dvdrental sample database. Below are the scenarios and solutions for the INNER JOIN queries requested in this assignment.
+
+## 📋 Project Requirements
+
+1) Write an INNER JOIN query to display city names (city) from the city table and country names (country) from the country table.
+
+2) Write an INNER JOIN query to display payment IDs (payment_id) from the payment table along with customer first names (first_name) and last names (last_name) from the customer table.
+
+3) Write an INNER JOIN query to display rental IDs (rental_id) from the rental table along with customer first names (first_name) and last names (last_name) from the customer table.
